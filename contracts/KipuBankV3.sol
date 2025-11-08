f34959a6907866459d82e3d0e7ca7b5fe20ad2df
MicaelaRasso <micaelarasso@mdp.edu.ar>
2025-11-08 19:30:15 -0300

    corrections on ETH handling

--- contracts/KipuBankV3.sol
+++ contracts/KipuBankV3.sol
@@ -6,9 +6,13 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IUniswapV2Router02} from "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import { IUniswapV2Factory } from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import { IUniswapV2Pair } from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+
 
 /*Interfaces*/
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 /*
@@ -18,6 +22,8 @@ import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/I
 @custom:security This is an educative contract and should not be used in production
 */
 contract KipuBankV3 is Ownable, ReentrancyGuard{
+    using SafeERC20 for IERC20;
+
 
 /*State variables*/
     ///@notice Mapping of user address to their balance in USDC.
@@ -38,12 +44,13 @@ contract KipuBankV3 is Ownable, ReentrancyGuard{
 
     /// @notice USDC ERC20 token instance.
     IERC20 public immutable i_usdc;
-    
-    /// @notice USDC ERC20 token instance.
-    IERC20 public immutable i_WETH;
+
+    address public immutable i_weth;
 
     /// @notice Uniswap V2 Router instance for token swaps.
     IUniswapV2Router02 public immutable uniswapRouter;
+
+    IUniswapV2Factory public uniswapFactory;
 
 /*Events*/
     /*
@@ -101,30 +108,30 @@ contract KipuBankV3 is Ownable, ReentrancyGuard{
     @dev Ensures that a withdrawal can only be made if it does not exceed the maximum allowed amount and the user has sufficient balance.
     @param amount The requested withdrawal amount.
     */
-    modifier _amountAvailable(uint256 amount, address token){
-        if(i_maxWithdrawal < amount) revert KipuBank_FailedWithdrawal("Amount exceeds the maximum withdrawal");
-        if(s_balances[msg.sender] < amount) revert KipuBank_InsufficientFounds("Not enough founds");
+    modifier _availableAmount(uint256 _amount){
+        if(i_maxWithdrawal < _amount) revert KipuBank_FailedWithdrawal("Amount exceeds the maximum withdrawal");
+        if(s_balances[msg.sender] < _amount) revert KipuBank_InsufficientFounds("Not enough founds");
         _;
     }
 
     modifier _areFundsExceeded(address _token, uint256 _amount){
-        uint256 amountInUSDC = _convertToUSDC(_token, _amount);
-        if (founds + amountInUSDC > i_bankCap) {
+        uint256 amountInUSDC = _estimateUSDCOut(_token, _amount);
+        if (IERC20(i_usdc).balanceOf(address(this)) + amountInUSDC > i_bankCap) {
             revert KipuBank_FailedDeposit("Total KipuBank's funds exceeded");
         }
         _;
     }
 
     modifier _onlySupportedToken(address _token){ //prevents using excesive gas in case of unsupported tokens
-        address pair = uniswapFactory.getPair(_token, i_usdc);
-            if(pair == address(0)){
+        address pair = uniswapFactory.getPair(_token, address(i_usdc));
+            if(pair == i_weth){
                 revert KipuBank_NotSupportedToken(_token);
             }
         _;
     }
 
     modifier _isTokenTransferAllowed(address _token, uint256 _amount) {
-        if (_token != address(0)) {
+        if (_token != i_weth) {
             IERC20 token = IERC20(_token);
             if (token.allowance(msg.sender, address(this)) < _amount) {
                 revert KipuBank_FailedOperation("ERC20: Insufficient allowance for transferFrom");
@@ -137,7 +144,7 @@ contract KipuBankV3 is Ownable, ReentrancyGuard{
 //constructor
     constructor(
         address _initialOwner, 
-        address _usdc, address _weth, address _router,
+        address _weth, address _usdc, address _router,
         uint256 _bankCap, uint256 _maxWithdrawal
         ) Ownable(_initialOwner)
         {
@@ -147,7 +154,10 @@ contract KipuBankV3 is Ownable, ReentrancyGuard{
         i_maxWithdrawal  = _maxWithdrawal* 10**8;
         
         i_usdc = IERC20(_usdc);
-        i_WETH = IERC20(_weth);
+        i_weth = _weth;
+
+        uniswapRouter = IUniswapV2Router02(_router);
+        uniswapFactory = IUniswapV2Factory(uniswapRouter.factory());
     }
 
 //receive & fallback
@@ -156,7 +166,7 @@ contract KipuBankV3 is Ownable, ReentrancyGuard{
     @dev Automatically calls the internal deposit function.
     */
     receive() external payable{
-        _depositEther(msg.sender, msg.value);
+        _depositETH(msg.sender, msg.value);
     }
 
     /*
@@ -181,24 +191,25 @@ contract KipuBankV3 is Ownable, ReentrancyGuard{
     @dev Uses _depositEther internal function and emits {KipuBank_SuccessfulDeposit}. 
     @custom:error KipuBank_FailedDeposit Thrown if the bank capacity is exceeded.
     */
-    function deposit(uint256 _amountMinOut) external payable nonReentrant{
-        _depositEther(msg.sender, msg.value, _amountMinOut);
+    function deposit() external payable nonReentrant{
+        _depositETH(msg.sender, msg.value);
     }
 
     function depositERC20(address _token, uint256 _amountIn, uint256 _amountMinOut
     ) external nonReentrant _onlySupportedToken(_token) _isTokenTransferAllowed(_token, _amountIn) _areFundsExceeded(_token, _amountIn){
-        IERC20(_token).transferFrom(msg.sender, address(this), _amountIn);
+
+        IERC20(_token).safeTransferFrom(msg.sender, address(this), _amountIn);
 
         uint256 beforeBalance = IERC20(i_usdc).balanceOf(address(this));
         uint256 usdcReceived;
 
-        if (_token != i_usdc) {
+        if (_token != address(i_usdc)) {
             IERC20(_token).approve(address(uniswapRouter), 0); // Reset to zero for safety
             IERC20(_token).approve(address(uniswapRouter), _amountIn);
 
-            address ;
+            address[] memory path ;
             path[0] = _token;
-            path[1] = i_usdc;
+            path[1] = address(i_usdc);
 
             uniswapRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(
                 _amountIn,
@@ -219,45 +230,50 @@ contract KipuBankV3 is Ownable, ReentrancyGuard{
         emit KipuBank_SuccessfulDeposit(msg.sender, usdcReceived);
     }
 
-    function withdrawUsdc(uint256 _amount) external nonReentrant _amountAvailable(_amount){
+    function withdrawUsdc(uint256 _amount) external nonReentrant _availableAmount(_amount){
         //Effects
         s_balances[msg.sender] -= _amount;
-        _actualizeOperations(false);
+        _actualizeOperations(false, address(i_usdc));
         //Interactions
-        emit KipuBank_SuccessfulWithdrawal(msg.sender, amount);
-        i_usdc.transfer(msg.sender, _amount);
+        emit KipuBank_SuccessfulWithdrawal(msg.sender, _amount);
+        i_usdc.safeTransfer(msg.sender, _amount);
     }
 
     function emergencyWithdrawal(uint256 _amount, address _recipient) external onlyOwner {
-        IERC20(i_usdc).transfer(_recipient, _amount);
+        IERC20(i_usdc).safeTransfer(_recipient, _amount);
     }
 
 //internal
-    function _convertToUSDC(address _token, uint256 _amount) internal view returns (uint256 usdcAmount) {
-        if (_token == i_usdc) {
-            return _amount;
-        }
-        AggregatorV3Interface priceFeed = tokenToUsdFeed[_token];
-        (, int256 price,,,) = priceFeed.latestRoundData();
+function _estimateUSDCOut(address _token, uint256 _amount) internal view returns (uint256) {
+    if (_token == address(i_usdc)) return _amount;
 
-        uint8 decimals = IERC20Metadata(_token).decimals();
-        usdcAmount = (_amount * uint256(price)) / (10 ** (decimals + 2)); 
-    }
+    address pair = uniswapFactory.getPair(_token, address(i_usdc));
+    require(pair != i_weth, "No pair");
 
-//private
-    
-    function _depositEther(address _address, uint256 _amount, uint256 _amountMinOut) private _areFundsExceeded(_amount, address(0)){
+    (uint112 reserve0, uint112 reserve1,) = IUniswapV2Pair(pair).getReserves();
+    (uint112 reserveIn, uint112 reserveOut) = _token < address(i_usdc) ? (reserve0, reserve1) : (reserve1, reserve0);
+
+    // Basic Uniswap constant product formula
+    uint256 amountInWithFee = _amount * 997; // 0.3% fee
+    uint256 numerator = amountInWithFee * reserveOut;
+    uint256 denominator = reserveIn * 1000 + amountInWithFee;
+    return numerator / denominator;
+}
+
+
+    //private
+        function _depositETH(address _sender, uint256 _amountIn) private _areFundsExceeded(i_weth, _amountIn) {
         uint256 beforeBalance = IERC20(i_usdc).balanceOf(address(this));
+        
+        uint256 _amountMinOut = _estimateUSDCOut(i_weth, _amountIn);
+        _amountMinOut /= 9 * 10;
 
-        address ;
-        path[0] = i_WETH;
-        path[1] = i_usdc;
+        address[] memory path;
+        path[0] = i_weth;
+        path[1] = address(i_usdc);
 
-        IWETH(i_WETH).deposit{value: msg.value}();
-        IERC20(i_WETH).approve(address(uniswapRouter), msg.value);
-
-        uniswapRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(
-            msg.value,
+        // call router: send ETH with the call
+        uniswapRouter.swapExactETHForTokensSupportingFeeOnTransferTokens{ value: _amountIn }(
             _amountMinOut,
             path,
             address(this),
@@ -266,11 +282,10 @@ contract KipuBankV3 is Ownable, ReentrancyGuard{
 
         uint256 afterBalance = IERC20(i_usdc).balanceOf(address(this));
         uint256 usdcReceived = afterBalance - beforeBalance;
-
-        s_balances[msg.sender] += usdcReceived;
-        _actualizeOperations(true, address(0));
-
-        emit KipuBank_SuccessfulDeposit(msg.sender, usdcReceived);
+        
+        s_balances[_sender] += usdcReceived;
+        _actualizeOperations(true, i_weth);
+        emit KipuBank_SuccessfulDeposit(_sender, usdcReceived);
     }
 
     function _actualizeOperations(bool isDeposit, address token) private{
